df384c685b8ebb9de28a01c231cb839741edf5ac
Orisabiyi <orisabiyidavid@gmail.com>
2024-01-02 22:44:15 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const headerName = document.querySelector('.header__name');
+const profileName = 'Orisabiyi David';
+let i = 0;
+
+
+const typingEffect = function () {
+  if (i < profileName.length) {
+    headerName.textContent += profileName[i];
+    i++;
+    setTimeout(typingEffect, 300);
+  }
+}
+
+typingEffect();
